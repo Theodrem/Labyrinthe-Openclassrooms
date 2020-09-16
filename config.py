cdafe5487445ca6a0ceb4439a0857cebebe82bdc
Theodrem <60725545+Theodrem@users.noreply.github.com>
2020-09-16 17:48:28 +0200

    first config

---
+++ config.py
@@ -0,0 +1,31 @@
+# coding: utf8
+
+"""
+controle perso
+"""
+
+right = "d"
+left = "g"
+high = "h"
+low = "b"
+inventoryButton = "i"
+
+"""
+message
+"""
+
+wall_message = "Attention au mur!"
+guardians_sleeping = "Bravo! Vous avez endormi le garde. Courez vers la sortie pour gagner!"
+
+guardian_loose = "Perdu! Vous n'avez pas ramassé tout les objets"
+winner = "Bravo! Vous avez terminé le labyrinthe."
+
+"""
+Sprites and maze properties
+"""
+wall = "O"
+way = " "
+mcgyver = "X"
+objects = "A", "B", "C", "D", "E"
+guardian = "G"
+exit = "U"
