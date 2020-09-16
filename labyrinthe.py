8d3e2340e7ba7c544ad9137ef0141995c4dba0d6
Theodrem <60725545+Theodrem@users.noreply.github.com>
2020-09-16 17:46:59 +0200

    first class Labyrinthe

---
+++ labyrinthe.py
@@ -0,0 +1,47 @@
+# coding: utf8
+import os
+import random
+from config import *
+
+
+class Labyrinthe:
+    def __init__(self, fichier):
+        self.structure = []
+        self.fichier = fichier
+        self.object = 0
+
+    def build(self):
+        print("Remplissage du labyrinthe....")
+
+        for filename in os.listdir("cartes"):
+            if filename.endswith(".txt"):
+                path = os.path.join("cartes", filename)
+                with open(path, "r") as file:
+                    content = file.readlines()
+                    maps = []
+
+                for row in content:
+                    line = []
+                    for character in row:
+                        line.append(character)
+                    maps.append(line)
+
+        self.structure = maps
+        self.object_placing()
+
+    def display(self):
+        for row in self.structure:
+            for character in row:
+                if character != ("\n"):
+                    print(character, end="")
+                else:
+                    print()
+
+    def object_placing(self):
+        number_objects = 0
+        while number_objects < 5:
+            randomX = random.randint(1, 10)
+            randomY = random.randint(1, 10)
+            if self.structure[randomX][randomY] == "O":
+                self.structure[randomX][randomY] = objects[number_objects]
+                number_objects += 1
