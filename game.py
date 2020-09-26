2b6eb79a4f9aa84779276e3a4cbaf252a2ad30d8
Theodrem <60725545+Theodrem@users.noreply.github.com>
2020-09-26 20:37:19 +0200

    maj game pygame

--- game.py
+++ game.py
@@ -2,41 +2,84 @@
 from player import Player
 from config import *
 from maze import Maze
+import pygame
 
 
 class GameStart:
     def __init__(self):
-        self.lab1 = 0
+        self.init = pygame.init()
+        self.window = pygame.display.set_mode((WIDTH_WINDOWS, HEIGHT_WINDOWS))
+        self.icon = pygame.display.set_icon(FLASK_2)
+        self.title = TITLE
+        self.maze = Maze()
+        self.player = Player(self.maze)
+        self.x = 0
+        self.y = 0
+
+    def display_maze(self):
+        for row in self.maze.structure:
+            for character in row:
+                if character != ("\n"):
+                    if character == WALL:
+                        self.window.blit(WALL_IMAGE, (self.maze.x, self.maze.y))
+                    elif character == PATH:
+                        self.window.blit(PATH_IMAGE, (self.maze.x, self.maze.y))
+                    elif character == MCGIVER:
+                        self.window.blit (MCGIVER_IMAGE, (self.maze.x, self.maze.y))
+                    elif character == OBJECT_1:
+                        self.window.blit(FLASK_1, (self.maze.x, self.maze.y))
+                    elif character == OBJECT_2:
+                        self.window.blit(FLASK_2, (self.maze.x, self.maze.y))
+                    elif character == OBJECT_3:
+                        self.window.blit(FLASK_3, (self.maze.x, self.maze.y))
+                    elif character == GUARD:
+                        self.window.blit(GUARD_IMAGE, (self.maze.x, self.maze.y))
+                    elif character == EXIT:
+                        self.window.blit(EXIT_IMAGE, (self.maze.x, self.maze.y))
+                    self.maze.x += 45
+                else:
+                    if self.maze.y <= HEIGHT_WINDOWS:
+                        self.maze.x = 0
+                        self.maze.y += 45
+                    else:
+                        self.maze.y = 0
 
     def jeu(self):
-        maze1 = Maze("prison.txt")
-        maze1.build()
-        maze1.display()
-        player1 = Player(maze1)
+        self.maze.build()
         continue_game = True
 
         while continue_game:
-            touch = input()
 
-            if touch == LOW:
-                player1.move(LOW)
+            self.display_maze()
+            pygame.display.flip()
 
-            elif touch == (HIGH):
-                player1.move(HIGH)
+            for events in pygame.event.get():
+                if events.type == pygame.QUIT:
+                    continue_game = False
+                    pygame.quit()
 
-            elif touch == (RIGHT):
-                player1.move(RIGHT)
+                elif events.type == pygame.KEYDOWN:
+                    if events.key == pygame.K_LEFT:
+                        self.player.move(LEFT)
 
-            elif touch == (LEFT):
-                player1.move(LEFT)
+                    elif events.key == pygame.K_RIGHT:
+                        self.player.move(RIGHT)
 
-            elif touch == (INVENTORY_BUTTON):
-                player1.move(INVENTORY_BUTTON)
+                    elif events.key == pygame.K_UP:
+                        self.player.move(UP)
 
-            else:
-                pass
+                    elif events.key == pygame.K_DOWN:
+                        self.player.move(DOWN)
 
-            if player1.win == True:
+            if self.player.win:
                 continue_game = False
+                pygame.quit()
+            elif self.player.lose:
+                continue_game = False
+                pygame.quit()
 
-            maze1.display()
+
+
+
+
+
