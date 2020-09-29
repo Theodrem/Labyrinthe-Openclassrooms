db2c88acf4ae42c80fd73caa832130db8c061cc8
Theodrem <60725545+Theodrem@users.noreply.github.com>
2020-09-29 13:43:15 +0200

    Maj 2 player.py

--- player.py
+++ player.py
@@ -14,63 +14,64 @@ class Player:
         self.objects = OBJECTS
         self.inventory = []
 
+    def respawn(self):
+        self.maze.structure[self.x][self.y] = MCGIVER
+
+    def attributes(self):
+        self.get_objects()
+        self.win_condition()
+        self.maze.structure[self.x][self.y] = MCGIVER
+
     def move(self, direction):
-        if direction == (DOWN):
+        if direction == DOWN:
             if self.maze.structure[self.x + 1][self.y] == WALL:
                 print(WALL_MESSAGE)
             elif self.maze.structure[self.x + 1][self.y] != WALL:
                 self.maze.structure[self.x][self.y] = PATH
                 self.x += 1
-                self.get_objects()
-                self.win_conditon()
-                self.maze.structure[self.x][self.y] = MCGIVER
+                self.attributes()
 
-        elif direction == (UP):
+        elif direction == UP:
             if self.maze.structure[self.x - 1][self.y] == WALL:
                 print(WALL_MESSAGE)
             elif self.maze.structure[self.x - 1][self.y] != WALL:
                 self.maze.structure[self.x][self.y] = PATH
                 self.x -= 1
-                self.get_objects()
-                self.win_conditon()
-                self.maze.structure[self.x][self.y] = MCGIVER
+                self.attributes()
 
-        elif direction == (RIGHT):
+        elif direction == RIGHT:
             if self.maze.structure[self.x][self.y + 1] == WALL:
                 print(WALL_MESSAGE)
             elif self.maze.structure[self.x][self.y + 1] != WALL:
                 self.maze.structure[self.x][self.y] = PATH
                 self.y += 1
-                self.get_objects()
-                self.win_conditon()
-                self.maze.structure[self.x][self.y] = MCGIVER
+                self.attributes()
 
-        elif direction == (LEFT):
+        elif direction == LEFT:
             if self.maze.structure[self.x][self.y - 1] == WALL:
                 print(WALL_MESSAGE)
             elif self.maze.structure[self.x][self.y - 1] != WALL:
                 self.maze.structure[self.x][self.y] = PATH
                 self.y -= 1
-                self.get_objects()
-                self.win_conditon()
-                self.maze.structure[self.x][self.y] = MCGIVER
-
-        elif direction == (INVENTORY_BUTTON):
-            print(len(self.inventory))
+                self.attributes()
 
     def get_objects(self):
         if self.maze.structure[self.x][self.y] in (self.objects):
             self.inventory.append(self.maze.structure[self.x][self.y])
 
-    def win_conditon(self):
+    def win_condition(self):
         if self.maze.structure[self.x][self.y] == (GUARD):
             if len(self.inventory) == 3:
                 self.maze.structure[self.x][self.y] = PATH
+
             else:
                 #lose
                 self.lose = True
+                self.inventory.clear()
+                self.respawn()
 
         if self.maze.structure[self.x][self.y] == (EXIT):
             if GUARD not in self.maze.structure:
                 print(WINNER)
                 self.win = True
+                self.inventory.clear()
