9221d0952d7f855105bdd7ed9497d03428aaa23a
Theodrem <60725545+Theodrem@users.noreply.github.com>
2020-09-29 13:42:59 +0200

    Maj 2 game.py

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ import pygame
 class GameStart:
     def __init__(self):
         self.init = pygame.init()
+        self.is_playing = False
         self.window = pygame.display.set_mode((WIDTH_WINDOWS, HEIGHT_WINDOWS))
         self.icon = pygame.display.set_icon(FLASK_2)
         self.title = TITLE
@@ -15,6 +16,21 @@ class GameStart:
         self.player = Player(self.maze)
         self.x = 0
         self.y = 0
+        self.play_button_rect = HOMEPAGE_BUTTON.get_rect()
+        self.play_button_rect.x = 160
+        self.play_button_rect.y = 470
+
+    def print(self, texte, x, y):
+        arial_font = pygame.font.SysFont("arial", 80)
+        blank = (255, 255, 255)
+        text = arial_font.render(str(texte), True, blank)
+        self.window.blit(text, [x, y])
+
+    def homepage(self, image):
+        self.window.blit(HOMEPAGE, (0, 0))
+        self.window.blit(HOMEPAGE_BANNER, (200, 200))
+        self.window.blit(image, (280, 50))
+        self.window.blit(HOMEPAGE_BUTTON, self.play_button_rect)
 
     def display_maze(self):
         for row in self.maze.structure:
@@ -25,7 +41,7 @@ class GameStart:
                     elif character == PATH:
                         self.window.blit(PATH_IMAGE, (self.maze.x, self.maze.y))
                     elif character == MCGIVER:
-                        self.window.blit (MCGIVER_IMAGE, (self.maze.x, self.maze.y))
+                        self.window.blit(MCGIVER_IMAGE, (self.maze.x, self.maze.y))
                     elif character == OBJECT_1:
                         self.window.blit(FLASK_1, (self.maze.x, self.maze.y))
                     elif character == OBJECT_2:
@@ -37,31 +53,50 @@ class GameStart:
                     elif character == EXIT:
                         self.window.blit(EXIT_IMAGE, (self.maze.x, self.maze.y))
                     self.maze.x += 45
+            else:
+                if self.maze.y <= HEIGHT_WINDOWS:
+                    self.maze.x = 0
+                    self.maze.y += 45
                 else:
-                    if self.maze.y <= HEIGHT_WINDOWS:
-                        self.maze.x = 0
-                        self.maze.y += 45
-                    else:
-                        self.maze.y = 0
+                    self.maze.y = 0
+
+    def refresh(self):
+        self.is_playing = False
+        self.maze.build()
+        self.player.x = 1
+        self.player.y = 1
 
     def jeu(self):
         self.maze.build()
+        self.homepage(WELCOME_IMAGE)
         continue_game = True
 
         while continue_game:
+            if not self.is_playing:
+                self.player.win = False
+                self.player.lose = False
+            else:
+                self.display_maze()
+                self.player.respawn()
+                self.print(len(self.player.inventory), 0, 0)
+                if self.player.win:
+                    self.refresh()
+                    self.homepage(WIN_IMAGE)
 
-            self.display_maze()
+                elif self.player.lose:
+                    self.refresh()
+                    self.homepage(GAME_OVER_IMAGE)
+
             pygame.display.flip()
 
             for events in pygame.event.get():
                 if events.type == pygame.QUIT:
                     continue_game = False
-                    pygame.quit()
+                    quit()
 
                 elif events.type == pygame.KEYDOWN:
                     if events.key == pygame.K_LEFT:
                         self.player.move(LEFT)
-
                     elif events.key == pygame.K_RIGHT:
                         self.player.move(RIGHT)
 
@@ -71,12 +106,11 @@ class GameStart:
                     elif events.key == pygame.K_DOWN:
                         self.player.move(DOWN)
 
-            if self.player.win:
-                continue_game = False
-                pygame.quit()
-            elif self.player.lose:
-                continue_game = False
-                pygame.quit()
+                elif events.type == pygame.MOUSEBUTTONDOWN:
+                    if self.play_button_rect.collidepoint(events.pos):
+                        self.is_playing = True
+
+
 
 
 
