262cf9eb989638b4610218953f7770c0ccd84dac
Theodrem <60725545+Theodrem@users.noreply.github.com>
2020-10-12 11:23:11 +0200

    maj 2 main.my

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 # coding: utf8
 from game import GameStart
-from config import *
-
-
 
 if __name__ == "__main__":
-    start = GameStart()
-    start.jeu()
+    launcher = GameStart()
+    launcher.loop()
