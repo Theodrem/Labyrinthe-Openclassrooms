767c5414a2103617971366afc39ac76a55fe92db
Theodrem <60725545+Theodrem@users.noreply.github.com>
2020-10-14 21:19:18 +0200

    add docstring

--- game.py
+++ game.py
@@ -5,39 +5,55 @@ from graphic import Graphic
 
 
 class GameStart:
+    """
+    link between classes: Maze, Player, Graphic
+    """
     def __init__(self):
         self.is_playing = False
         self.graphic = Graphic()
         self.player = Player(self.graphic.maze)
         self.continue_game = True
+        self.size_inventory = len(self.player.inventory)
 
-    def refresh(self):
+    def start(self):
+        """
+       Start the game, building the maze, placement of the player
+        """
         self.is_playing = False
         self.graphic.maze.build()
         self.player.x = 1
         self.player.y = 1
 
-    def start(self):
-        self.graphic.maze.build()
-        self.graphic.homepage(WELCOME_IMAGE)
-
     def not_playing(self):
+        """
+        On the home page, game at the stop
+        """
         self.player.win = False
         self.player.lose = False
 
     def playing(self):
+        """
+        dysplay the maze,
+        respawn the player,
+        display the counter of inventory
+        if the player wins or loses the game is restarted, and display on home page won image or game over image
+        """
         self.graphic.display_maze()
         self.player.spawn()
         self.graphic.print(len(self.player.inventory), 0, 0)
         if self.player.win:
-            self.refresh()
+            self.start()
             self.graphic.homepage(WIN_IMAGE)
 
         elif self.player.lose:
-            self.refresh()
+            self.start()
             self.graphic.homepage(GAME_OVER_IMAGE)
 
     def action(self):
+        """
+        detection of keys pressed by the player,
+        close button or play button and directional keys
+        """
         for events in pygame.event.get():
             if events.type == pygame.QUIT:
                 self.continue_game = False
@@ -60,12 +76,19 @@ class GameStart:
                     self.is_playing = True
 
     def loop(self):
+        """
+        home screen,
+        Starting the game,
+        loop of the part
+        end of the game with victory or defeat
+        """
         self.start()
+        self.graphic.homepage(WELCOME_IMAGE)
         while self.continue_game:
-            if not self.is_playing:
-                self.not_playing()
-            else:
+            if self.is_playing:
                 self.playing()
+            else:
+                self.not_playing()
             self.graphic.flip()
             self.action()
 
