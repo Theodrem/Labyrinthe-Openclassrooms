b2db92f8240add705ad65af9b3ae60feceacc67a
Theodrem <60725545+Theodrem@users.noreply.github.com>
2020-10-14 21:19:29 +0200

    add docstring

--- graphic.py
+++ graphic.py
@@ -4,6 +4,10 @@ from maze import Maze
 
 
 class Graphic:
+    """
+    Graphic elements of the game with pygame,
+    homepage, maze, object counter,
+    """
     def __init__(self):
         pygame.init()
         self.window = pygame.display.set_mode((WIDTH_WINDOWS, HEIGHT_WINDOWS))
@@ -14,22 +18,35 @@ class Graphic:
         self.maze = Maze()
 
     def print(self, text, x, y):
+        """
+        display object counter
+
+        :param x: position x
+        :param y: position y
+
+        """
         arial_font = pygame.font.SysFont("arial", 80)
         blank = (255, 255, 255)
         text = arial_font.render(str(text), True, blank)
         self.window.blit(text, [x, y])
 
     def homepage(self, image):
+        """
+        home page display, play button, welcome_image or win_image or game_over image
+        """
         self.window.blit(HOMEPAGE, (0, 0))
         self.window.blit(HOMEPAGE_BANNER, (200, 200))
         self.window.blit(image, (280, 50))
         self.window.blit(HOMEPAGE_BUTTON, self.play_button_rect)
 
     def display_maze(self):
+        """
+        for each character in structure display the corresponding image
+        """
         for row in self.maze.structure:
             for character in row:
                 if character != ("\n"):
-                    self.window.blit((DICTIONNARY[character]), (self.maze.x, self.maze.y))
+                    self.window.blit((DICTIONNARY_IMAGE[character]), (self.maze.x, self.maze.y))
                 self.maze.x += 45
             else:
                 if self.maze.y <= HEIGHT_WINDOWS:
@@ -40,4 +57,7 @@ class Graphic:
                     self.maze.x = 0
 
     def flip(self):
+        """
+        refresh page
+        """
         pygame.display.flip()
