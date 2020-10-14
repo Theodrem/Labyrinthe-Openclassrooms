5f84e037a22797487a55ff22cb15ba5428744ad8
Theodrem <60725545+Theodrem@users.noreply.github.com>
2020-10-14 21:20:43 +0200

    add docstring

--- maze.py
+++ maze.py
@@ -5,12 +5,20 @@ from config import *
 
 
 class Maze:
+    """
+    construction of the maze and placement of the objects.
+    """
     def __init__(self):
         self.structure = []
         self.x = 0
         self.y = 0
 
     def build(self):
+        """
+        open the cartes directory, loop on the files.
+        read lines in prison.txt and for each character add
+        in the list
+        """
         for filename in os.listdir(DIRECTORY_MAP):
             if filename.endswith(".txt"):
                 path = os.path.join(DIRECTORY_MAP, filename)
@@ -28,6 +36,9 @@ class Maze:
         self.object_placing()
 
     def object_placing(self):
+        """
+        while number_object not equal 3 add object in strcuture has a random position
+        """
         number_objects = 0
         while number_objects < 3:
             random_x = random.randint(1, 10)
