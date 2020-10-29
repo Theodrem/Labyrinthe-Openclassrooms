b114d66a49305bd9f77040218f2ba99e309ab436
Theodrem <60725545+Theodrem@users.noreply.github.com>
2020-10-29 01:25:01 +0100

    update Maze 1.6

--- config.py
+++ config.py
@@ -32,8 +32,10 @@ EXIT = "U"
 """
 Properties pygame front
 """
-WIDTH_WINDOWS = 675
-HEIGHT_WINDOWS = 675
+WIDTH_WINDOWS = 600
+HEIGHT_WINDOWS = 600
+
+SIZE_SPRITE = 40
 
 """
 image pygame
@@ -57,22 +59,22 @@ WELCOME_IMAGE = pygame.image.load("assets/welcome.png")
 WELCOME_IMAGE = pygame.transform.scale(WELCOME_IMAGE, (150, 150))
 
 WALL_IMAGE = pygame.image.load("assets/mur.png")
-WALL_IMAGE = pygame.transform.scale(WALL_IMAGE, (45, 45))
+WALL_IMAGE = pygame.transform.scale(WALL_IMAGE, (SIZE_SPRITE, SIZE_SPRITE))
 
 PATH_IMAGE = pygame.image.load("assets/fond.jpg")
-PATH_IMAGE = pygame.transform.scale(PATH_IMAGE, (45, 45))
+PATH_IMAGE = pygame.transform.scale(PATH_IMAGE, (SIZE_SPRITE, SIZE_SPRITE))
 
 MCGIVER_IMAGE = pygame.image.load("assets/scientist.png")
-MCGIVER_IMAGE = pygame.transform.scale(MCGIVER_IMAGE, (45, 45))
+MCGIVER_IMAGE = pygame.transform.scale(MCGIVER_IMAGE, (SIZE_SPRITE, SIZE_SPRITE))
 
 GUARD_IMAGE = pygame.image.load("assets/keeper.png")
-GUARD_IMAGE = pygame.transform.scale(GUARD_IMAGE, (45, 45))
+GUARD_IMAGE = pygame.transform.scale(GUARD_IMAGE, (SIZE_SPRITE, SIZE_SPRITE))
 
 EXIT_IMAGE = pygame.image.load("assets/exit.png")
-EXIT_IMAGE = pygame.transform.scale(EXIT_IMAGE, (45, 45))
+EXIT_IMAGE = pygame.transform.scale(EXIT_IMAGE, (SIZE_SPRITE, SIZE_SPRITE))
 
 FLASK_1 = pygame.image.load("assets/flask.png")
-FLASK_1 = pygame.transform.scale(FLASK_1, (45, 45))
+FLASK_1 = pygame.transform.scale(FLASK_1, (SIZE_SPRITE, SIZE_SPRITE))
 
 FLASK_2 = pygame.image.load("assets/flask2.png")
 FLASK_2 = pygame.transform.scale(FLASK_2, (45, 45))
@@ -83,12 +85,12 @@ FLASK_3 = pygame.transform.scale(FLASK_3, (45, 45))
 """
 button properties
 """
-BUTTON_X = 160
-BUTTON_Y = 470
+BUTTON_X = 120
+BUTTON_Y = 400
 
 
 DICTIONNARY_IMAGE = {
-    WALL:WALL_IMAGE,
+    WALL: WALL_IMAGE,
     PATH: PATH_IMAGE,
     GUARD: GUARD_IMAGE,
     OBJECT_1: FLASK_1,
